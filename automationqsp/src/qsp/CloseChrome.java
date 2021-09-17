236934a838241116a248dc5dd330f55622b1c3a1
divya99qa <90824828+divya99qa@users.noreply.github.com>
2021-09-17 09:30:12 +0530

    new update

--- automationqsp/src/qsp/CloseChrome.java
+++ automationqsp/src/qsp/CloseChrome.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CloseChrome {
 
 	public static void main(String[] args) throws InterruptedException{
-		System.out.println("Close Chrome");
+		System.out.println("Close Chrome browser");
 		System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
 		ChromeDriver driver=new ChromeDriver();
 		Thread.sleep(5000);
